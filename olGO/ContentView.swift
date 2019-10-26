a5399257de64521f4e75354dfb8bc8be0f667449
zmeriksen <zmeriksen@gmail.com>
2019-10-26 17:11:44 -0500

    Initial Commit

---
+++ olGO/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  olGO
+//
+//  Created by Zach Eriksen on 10/26/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
